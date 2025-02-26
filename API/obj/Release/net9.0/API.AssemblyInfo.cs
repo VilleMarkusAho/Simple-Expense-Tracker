c3df12f7f17cd40d9531fc1497bce9db332feb29
Ville-Markus Aho <villemarkus.aho1994@outlook.com>
2025-02-26 19:21:04 +0200

    Add new configurations and update assembly info
    - Updated `API.csproj` to include `Debug` and `Release` configurations.
    - Added runtime version comment and updated `AssemblyInformationalVersion` in `API.AssemblyInfo.cs`.
    - Modified `API.AssemblyInfoInputs.cache` with new hash values.
    - Introduced a new `Development` configuration in `Simple Expense Tracker.sln` with associated build settings.

--- API/obj/Release/net9.0/API.AssemblyInfo.cs
+++ API/obj/Release/net9.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7fe5e2293d2bb846791119b0c863f4e36cb68af")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
