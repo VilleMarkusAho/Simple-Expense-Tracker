03d797635c6fbe6d31745e03499567f417a827c2
Ville-Markus Aho <villemarkus.aho1994@outlook.com>
2025-02-26 19:22:03 +0200

    Update assembly informational version and cache hash
    Updated the `AssemblyInformationalVersionAttribute` in
    `API.AssemblyInfo.cs` to reflect a new version. Also changed
    the hash value in `API.AssemblyInfoInputs.cache` to
    ensure the cache is in sync with the updated assembly information.

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7fe5e2293d2bb846791119b0c863f4e36cb68af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3df12f7f17cd40d9531fc1497bce9db332feb29")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
