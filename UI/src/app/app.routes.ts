1108996c905e03790426a7c453a2235be3edbd17
Ville-Markus Aho <villemarkus.aho1994@outlook.com>
2025-02-26 20:17:09 +0200

    Add login page component with routing and styles

--- UI/src/app/app.routes.ts
+++ UI/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' }, // Default route
+    { path: 'login', loadComponent: () => import('./pages/login-page/login-page.component').then(m => m.LoginPageComponent) },
+];
