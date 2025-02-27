70a2e9bd1bf2bab775f10f8ce09c876afa30d1b0
Ville-Markus Aho <villemarkus.aho1994@outlook.com>
2025-02-27 18:27:09 +0200

    Bootstrap installed and login page template created

--- UI/src/app/pages/login-page/login-page.component.ts
+++ UI/src/app/pages/login-page/login-page.component.ts
@@ -1,14 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css'],
+  imports: [CommonModule, FormsModule],
   standalone: true
 })
 export class LoginPageComponent implements OnInit {
 
   constructor() { }
+
+  username: string = "";
+  password: string = "";
 
   ngOnInit() {
   }
