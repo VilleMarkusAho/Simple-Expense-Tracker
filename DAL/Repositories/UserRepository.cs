7eae598aff0951b69dd00cf085956b24cc897d22
Ville-Markus Aho <villemarkus.aho1994@outlook.com>
2025-03-01 00:16:19 +0200

    Add user configuration and SQLite support
    Updated appsettings.json to include EXAMPLE_USER credentials.
    Modified DAL.csproj to add package references for
    Microsoft.Extensions.Configuration and System.Data.SQLite.
    Enhanced UserRepository to manage SQLite connection and
    create a Users table, utilizing configuration for default user.

--- DAL/Repositories/UserRepository.cs
+++ DAL/Repositories/UserRepository.cs
@@ -4,11 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DAL.Models;
+using System.Data.SQLite;
+using Microsoft.Extensions.Configuration;
 
 namespace DAL.Repositories
 {
     public class UserRepository : IRepository<User>
     {
+        private readonly SQLiteConnection _connection;
+        private readonly IConfiguration _configuration;
+
+        public UserRepository(string connectionString, IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+            _connection = new SQLiteConnection(connectionString);
+            _connection.Open();
+
+            CreateTable();
+        }
+
+        private void CreateTable()
+        {
+            string query = @"
+                CREATE TABLE IF NOT EXISTS Users (
+                    UserId INTEGER PRIMARY KEY AUTOINCREMENT, 
+                    Username VARCHAR(20) UNIQUE, 
+                    Password VARCHAR(32), 
+                    FirstName TEXT, 
+                    LastName TEXT";
+
+            using var cmd = new SQLiteCommand(query, _connection);
+            cmd.ExecuteNonQuery();
+
+            // Seed the database with a default user
+            var defaultUser = _configuration.GetSection("EXAMPLE_USER") as User;
+
+
+
+        }
+
         public Task<User> GetByIdAsync(int id)
         {
             throw new NotImplementedException();
