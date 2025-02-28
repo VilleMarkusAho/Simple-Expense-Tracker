e431bcadf5b2e530c5b0a7fb7054ec8e93030c56
Ville-Markus Aho <villemarkus.aho1994@outlook.com>
2025-02-28 18:41:43 +0200

    Refactor project settings and enhance login functionality
    - Updated `API.csproj` to fix formatting in `Configurations`.
    - Changed active debug profile from `https` to `http` in `API.csproj.user` and added a new property group for debugging.
    - Introduced `LoginRequest` class and `Login` method in `LoginController.cs` for handling login requests.
    - Added CORS support in `Program.cs` with a policy for allowing all origins.
    - Restructured `launchSettings.json` to simplify profiles and set application URLs to port `5000`.

--- API/Controllers/LoginController.cs
+++ API/Controllers/LoginController.cs
@@ -3,9 +3,22 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
+    public class LoginRequest
+    {
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+    }
+
+    [Produces("application/json")]
     [Route("api/[controller]")]
     [ApiController]
     public class LoginController : ControllerBase
     {
+
+        [HttpPost]
+        public IActionResult Login(LoginRequest request)
+        {
+            return Ok();
+        }
     }
 }
