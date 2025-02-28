d7ed5486b7d04c15a64b89ec63e3e65805fabe57
Ville-Markus Aho <villemarkus.aho1994@outlook.com>
2025-02-28 17:52:12 +0200

    Add Angular Material and update login page with new features and styles

--- UI/src/app/app.config.ts
+++ UI/src/app/app.config.ts
@@ -1,12 +1,12 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { HttpInterceptorService } from './services/HttpInterceptor.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    HttpClient,
+    provideHttpClient(withInterceptorsFromDi()),
     { provide: HTTP_INTERCEPTORS, useClass: HttpInterceptorService, multi: true },
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
--- UI/src/app/pages/login-page/login-page.component.ts
+++ UI/src/app/pages/login-page/login-page.component.ts
@@ -1,22 +1,35 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../services/AuthService.service';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css'],
   imports: [CommonModule, FormsModule],
+  providers: [AuthService],
   standalone: true
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   username: string = "";
   password: string = "";
 
-  ngOnInit() {
+  signningIn: boolean = false;
+
+  login(): void {
+
+  }
+
+  onFogotPasswordClick(): void {
+    alert("Nothing happens");
+  }
+
+  ngOnInit(): void {
+
   }
 
 }
