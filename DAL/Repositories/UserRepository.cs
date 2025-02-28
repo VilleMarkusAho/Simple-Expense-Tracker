08ae1f1381433da515a3f3d633f424bcec715460
Ville-Markus Aho <villemarkus.aho1994@outlook.com>
2025-02-28 23:38:49 +0200

    Add DAL project with User model and repository interface
    Created `DAL.csproj` targeting .NET 9.0 with implicit usings and nullable reference types. Added `User` class with properties for user details. Introduced `IRepository<T>` interface for CRUD operations. Implemented `UserRepository` class with methods throwing `NotImplementedException`. Updated solution file to include the new `DAL` project with build configurations.

---
+++ DAL/Repositories/UserRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Models;
+
+namespace DAL.Repositories
+{
+    public class UserRepository : IRepository<User>
+    {
+        public Task<User> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<User>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> AddAsync(User entity)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<User> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+       
+        public Task<User> UpdateAsync(User entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
