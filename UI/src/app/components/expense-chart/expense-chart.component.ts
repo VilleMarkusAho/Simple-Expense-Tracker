a885696f9d7991095d59e59fc155de07dcdd1acc
Ville-Markus Aho <villemarkus.aho1994@outlook.com>
2025-03-04 18:53:05 +0200

    Implement ExpenseChart component with Chart.js integration; update template and styles for improved rendering

--- UI/src/app/components/expense-chart/expense-chart.component.ts
+++ UI/src/app/components/expense-chart/expense-chart.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-expense-chart',
@@ -7,6 +8,40 @@ import { Component } from '@angular/core';
   styleUrl: './expense-chart.component.scss',
   standalone: true
 })
-export class ExpenseChartComponent {
+export class ExpenseChartComponent implements OnInit {
+  chart: any;
 
+  createChart(){
+
+    this.chart = new Chart("MyChart", {
+      type: 'bar', //this denotes tha type of chart
+
+      data: {// values on X-Axis
+        labels: ['2022-05-10', '2022-05-11', '2022-05-12','2022-05-13',
+                                 '2022-05-14', '2022-05-15', '2022-05-16','2022-05-17', ],
+           datasets: [
+          {
+            label: "Sales",
+            data: ['467','576', '572', '79', '92',
+                                 '574', '573', '576'],
+            backgroundColor: 'blue'
+          },
+          {
+            label: "Profit",
+            data: ['542', '542', '536', '327', '17',
+                                     '0.00', '538', '541'],
+            backgroundColor: 'limegreen'
+          }
+        ]
+      },
+      options: {
+        aspectRatio:2.5
+      }
+
+    });
+  }
+
+  ngOnInit(): void {
+    this.createChart();
+  }
 }
