ec8ac46b2dcd15b22e86ac0c20663d57459005d6
Ville-Markus Aho <villemarkus.aho1994@outlook.com>
2025-03-04 22:51:47 +0200

    Refactor ExpenseChartComponent: update chart labels, enhance dataset handling, and calculate total expenses for improved data representation

--- UI/src/app/components/expense-chart/expense-chart.component.ts
+++ UI/src/app/components/expense-chart/expense-chart.component.ts
@@ -23,7 +23,7 @@ export class ExpenseChartComponent implements OnChanges {
     this.chart = new Chart("Expenses", {
       type: 'bar',
       data: {
-        labels: ["Category"],
+        labels: ["Expenses by category"],
         datasets: this.getChartDatasets(),
       },
       options: {
@@ -45,33 +45,44 @@ export class ExpenseChartComponent implements OnChanges {
   private getChartDatasets(): any[] {
     let datasets1 = [];
     const datasets2 = [];
+    let totalExpenses = 0;
 
     for (const expense of this.expenses) {
       datasets1.push({
-        label: expense.category,
+        label: expense.category.toString(),
         data: [expense.amount],
+        stack: expense.category,
       });
 
-      datasets2.push(expense.amount);
+      datasets2.push({
+        label: expense.category.toString(),
+        data: [expense.amount],
+        stack: 'stack1',
+      })
+
+      totalExpenses += expense.amount;
     }
 
+    datasets1 = [...datasets1, ...datasets2];
 
     datasets1.push({
-      label: "Total expenses",
-      data: datasets2,
-    });
+      label: "Revenue",
+      data: [this.revenue],
+      stack: "stack2",
+    })
 
-    console.log(datasets1);
-
+    datasets1.push({
+      label: "Profit",
+      data: [this.revenue - totalExpenses],
+      stack: "stack2",
+    })
 
     return datasets1;
   }
 
   private getLabels(): string[] {
-    const categories = this.expenses.map((expense) => expense.category);
-
     return [
-      ... new Set(categories),
+      ...this.expenses.map((expense) => expense.category),
       "Total expenses",
       "Revenue",
       "Profit",
