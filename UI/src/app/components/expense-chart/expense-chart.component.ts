3ffecc6b65be6f6b7f57cc1b72e3b4a4913eda25
Ville-Markus Aho <villemarkus.aho1994@outlook.com>
2025-03-04 18:04:17 +0200

    Add ExpenseChart component and integrate it into the dashboard; update package.json and package-lock.json to include chart.js

---
+++ UI/src/app/components/expense-chart/expense-chart.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-expense-chart',
+  imports: [],
+  templateUrl: './expense-chart.component.html',
+  styleUrl: './expense-chart.component.scss',
+  standalone: true
+})
+export class ExpenseChartComponent {
+
+}
--- UI/src/app/pages/dashboard/dashboard.component.ts
+++ UI/src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ExpenseSpreadsheetComponent } from "../../components/expense-spreadsheet/expense-spreadsheet.component";
 import { DownloadPanelComponent } from '../../components/download-panel/download-panel.component';
+import { ExpenseChartComponent } from "../../components/expense-chart/expense-chart.component";
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
-  imports: [ExpenseSpreadsheetComponent, DownloadPanelComponent]
+  imports: [ExpenseSpreadsheetComponent, DownloadPanelComponent, ExpenseChartComponent]
 })
 export class DashboardComponent implements OnInit {
 
