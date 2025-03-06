3b5a9e36b556d9774f2e62f7e15f5cc95acdf1b9
Ville-Markus Aho <villemarkus.aho1994@outlook.com>
2025-03-06 23:02:08 +0200

    Update ProfileEditPageComponent: add back button with routing and remove unused AuthService import

--- UI/src/app/pages/profile-edit-page/profile-edit-page.component.ts
+++ UI/src/app/pages/profile-edit-page/profile-edit-page.component.ts
@@ -2,18 +2,18 @@ import { LocalStorageService } from './../../services/LocalStorage.service';
 import { Component, OnInit } from '@angular/core';
 import { UserFormComponent } from '../../components/user-form/user-form.component';
 import { UserForm } from '../../models/user.model';
-import { AuthService } from '../../services/AuthService.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-profile-edit-page',
   templateUrl: './profile-edit-page.component.html',
   styleUrls: ['./profile-edit-page.component.css'],
-  imports: [UserFormComponent],
+  imports: [UserFormComponent, RouterModule],
   standalone: true
 })
 export class ProfileEditPageComponent implements OnInit {
 
-  constructor(private localStorage: LocalStorageService, private authService: AuthService) {
+  constructor(private localStorage: LocalStorageService) {
     const user = this.localStorage.getUser();
     this.user = new UserForm(user);
   }
